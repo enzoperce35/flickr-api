44f0cec2dd4f94f6c245e969508d17cf168d7fb5
enzoperce35 <enzoperce35@gmail.com>
2022-01-11 07:25:14 +0800

    project finished

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,11 @@ class StaticPagesController < ApplicationController
   end
 
   def show
-    @flickr_id = params[:flickr_id]
+    flickr_id = params[:flickr_id]
+
+    flickr = Flickr.new
+
+    @photos = flickr.photos.search(:user_id => flickr_id.values[0], :api_key => ENV['FLICKR_API_KEY'])
+
   end
 end
